a00c59d808275cffbdd6f0e08079a34a11c90d0e
Mateusz Włusek <mateusz.wlusek@gmail.com>
2020-05-25 20:47:19 +0200

    3.1-Pulpit

---
+++ development/js/app.js
@@ -0,0 +1,60 @@
+const recipeAddButton = document.querySelector(".button--recipe-add");
+const planAddButton = document.querySelector(".button--plan-add");
+const widgetCloseButton = document.querySelectorAll(".division--close-button");
+const divisionsWidgetWindow = document.querySelectorAll("[class*=divisions--]");
+const weeknumspan = document.querySelector(".weekplan--info_weeknum");
+const prevWeekButton = document.querySelector(".weekplan--buttons-prevweek");
+const nextWeekButton = document.querySelector(".weekplan--buttons-nextweek");
+
+console.log(
+  recipeAddButton,
+  planAddButton,
+  widgetCloseButton,
+  divisionsWidgetWindow
+);
+
+// Te przyciski do dorobienia jak będzie dodawanie przepisu i dodawanie planu
+// recipeAddButton.addEventListener('click', function () {
+//     if (windowpopupname.style.display = none) {
+//         windowpopupname.style.display = block;
+//     }
+// });
+
+// planAddButton.addEventListener('click', function () {
+//     if (windowpopupname2.style.display = none) {
+//         windowpopupname2.style.display = block;
+//     }
+// });
+
+//Opcja z forEach...
+// widgetCloseButton.forEach(function (e) {
+//     e.addEventListener('click', function () {
+//         e.parentElement.style.display = 'none';
+//     });
+// })
+
+for (let i = 0; i < widgetCloseButton.length; i++) {
+  widgetCloseButton[i].addEventListener("click", function (e) {
+    this.parentElement.style.display = "none";
+  });
+}
+
+//Obliczanie tygodnia
+const now = new Date();
+const start = new Date(now.getFullYear(), 0, 0);
+const diff =
+  now -
+  start +
+  (start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000;
+const oneday = 1000 * 60 * 60 * 24;
+let numberofweek = Math.floor(diff / oneday / 7);
+
+prevWeekButton.addEventListener("click", function () {
+  numberofweek--;
+  weeknumspan.innerText = numberofweek;
+});
+nextWeekButton.addEventListener("click", function () {
+  numberofweek++;
+  weeknumspan.innerText = numberofweek;
+});
+weeknumspan.innerText = numberofweek;
